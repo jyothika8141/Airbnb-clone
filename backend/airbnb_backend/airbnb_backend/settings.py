b29262439e676e7673509957998614a935e1da03
jyothika <jyothika8141@outlook.com>
2024-08-23 17:21:49 +0530

    <Day-6>--<Django and docker installation(Redo-initial installation did not work)>

--- backend/airbnb_backend/airbnb_backend/settings.py
+++ backend/airbnb_backend/airbnb_backend/settings.py
@@ -5,17 +5,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG', default=0))
+DEBUG = bool(os.environ.get('DEBUG', default=False))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -65,8 +61,12 @@ WSGI_APPLICATION = 'airbnb_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('SQL_ENGINE'),
+        'NAME': os.environ.get('SQL_DATABASE'),
+        'USER': os.environ.get('SQL_USER'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD'),
+        'HOST': os.environ.get('SQL_HOST'),
+        'PORT': os.environ.get('SQL_PORT'),
     }
 }
 
---
+++ backend/airbnb_backend/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Check if database is running..."
+
+    # Wait until the PostgreSQL server is available
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "The database is up and running :D"
+fi
+
+# Run Django migrations
+python manage.py migrate
+
+# Execute the CMD from Dockerfile
+exec "$@"
